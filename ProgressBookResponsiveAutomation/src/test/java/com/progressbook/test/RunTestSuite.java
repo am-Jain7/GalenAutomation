059bcf1dfe189cc17d010475c537ef803ecf7056
ThakurA <ajay.thakur1@globallogic.com>
2020-01-03 15:36:08 +0530

    Test runner file added

---
+++ ProgressBookResponsiveAutomation/src/test/java/com/progressbook/test/RunTestSuite.java
@@ -0,0 +1,9 @@
+package com.progressbook.test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({GalenLoginPageTest.class,GalenHomePageTest.class})
+public class RunTestSuite {
+}
