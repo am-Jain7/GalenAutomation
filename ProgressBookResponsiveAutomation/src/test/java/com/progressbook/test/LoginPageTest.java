553af9da51518faabfe163692761512221832f2c
ThakurA <ajay.thakur1@globallogic.com>
2020-01-03 10:54:48 +0530

    updated

--- ProgressBookResponsiveAutomation/src/test/java/com/progressbook/test/LoginPageTest.java
+++ ProgressBookResponsiveAutomation/src/test/java/com/progressbook/test/LoginPageTest.java
@@ -3,31 +3,37 @@ import com.galenframework.api.Galen;
 import com.galenframework.reports.GalenTestInfo;
 import com.galenframework.reports.HtmlReportBuilder;
 import com.galenframework.reports.model.LayoutReport;
+
+import org.junit.After;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class LoginPageTest
 {
-    public WebDriver driver;
+    private WebDriver driver;
 
     @Before
     public void setUp()
     {
       
         driver = new ChromeDriver();
-//        Set the browser size for desktop    	
-        driver.manage().window().setSize(new Dimension(1200, 800));
-        //Go to swtestacademy.com
+//        Set the browser size for desktop   
+      driver.manage().window().maximize();
+//        driver.manage().window().setSize(new Dimension(1200, 800));
         driver.get("https://demo-ca.progressbook.com/");
     }
 
@@ -36,16 +42,16 @@ public class LoginPageTest
     {
 //        Create a layoutReport object
 //        checkLayout function checks the layout and returns a LayoutReport object
-        LayoutReport layoutReport = Galen.checkLayout(driver, "specs/homepage.gspec", Arrays.asList("desktop"));
+        LayoutReport layoutReport = Galen.checkLayout(driver, "specs/LoginPage.gspec", Arrays.asList("desktop"));
 
         //Create a tests list
         List<GalenTestInfo> tests = new LinkedList<GalenTestInfo>();
 
         //Create a GalenTestInfo object
-        GalenTestInfo test = GalenTestInfo.fromString("homepage layout");
+        GalenTestInfo test = GalenTestInfo.fromString("Login page layout");
 
         //Get layoutReport and assign to test object
-        test.getReport().layout(layoutReport, "check homepage layout");
+        test.getReport().layout(layoutReport, "Check login page layout");
 
         //Add test object to the tests list
         tests.add(test);
@@ -70,3 +76,6 @@ public class LoginPageTest
     }
 
 }
+
+
+ 
